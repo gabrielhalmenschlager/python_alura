6741ba8c78fce6e7aa6563f956cd427669768681
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-07-25 22:22:55 -0300

    Laço de repetição for

--- sabor_express/app.py
+++ sabor_express/app.py
@@ -1,6 +1,6 @@
 import os
 
-restaurantes = []
+restaurantes = ['Pizza', 'Humburger']
 
 def exibir_nome_do_programa():      
     print('''
@@ -24,7 +24,7 @@ def escolher_opcao():
         if opcao_escolhida ==  1:
             cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
-            listar_restaurante()
+            listar_restaurantes()
         elif opcao_escolhida == 3:
             ativar_restaurante()
         elif opcao_escolhida == 4:
@@ -39,24 +39,20 @@ def cadastrar_novo_restaurante():
     print('Cadastro de novos restaurantes\n')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
-    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!\n')
-    input('Digite uma tecla para voltar ao menu principal: ')
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    input('\nDigite uma tecla para voltar ao menu principal: ')
     main()
 
-def listar_restaurante():
-    print('Listar restaurante\n')
+def listar_restaurantes():
+    os.system('cls')
+    print('Listando os restaurantes\n')
+    for restaurante in restaurantes:
+        print(f'- {restaurante}')
+    input('\nDigite uma tecla para voltar ao menu principal: ')
+    main()
 
 def ativar_restaurante():
     print('Ativar restaurante\n')
-
-def finalizar_app():
-    os.system('cls')
-    print('Finalizando o app\n')
-
-def opcao_invalida():
-    print('Opção inválida!\n')
-    input('Digite uma tecla para voltar ao menu principal: ')
-    main()
 
 def main():
     os.system('cls')
